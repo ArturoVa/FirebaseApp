21cd86580758e7d38993211b3540c86b7bf52717
Alejandro Sauqué Pérez <alejandro.sauqueprz@uanl.edu.mx>
2023-08-30 17:27:17 -0600

    Initial commit

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDBw8m-CaUL2cr5Qbh2joHAf-U3AhKDoSQ',
+  authDomain: 'cursodefirebase-e1c2d.firebaseapp.com',
+  projectId: 'cursodefirebase-e1c2d',
+  storageBucket: 'cursodefirebase-e1c2d.appspot.com',
+  messagingSenderId: '990793969518',
+  appId: '1:990793969518:web:d14edf0b7b37045a851b7f'
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
---
+++ src/pages/Login/LoginPage.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>Login</h1>
+  <div class="credentials-container">
+    <label for="email">Email</label>
+    <input type="email" />
+    <label for="password">Contraseña</label>
+    <input type="password" />
+  </div>
+  <button>Continuar</button>
+</template>
+
+<style scoped>
+h1 {
+  text-align: center;
+}
+.credentials-container {
+  padding-top: 10px;
+  padding-bottom: 10px;
+  margin: auto;
+  width: 20%;
+  max-width: 1200px;
+  background-color: whitesmoke;
+  border: 0.5px solid gray;
+  border-radius: 10px;
+}
+label,
+input {
+  margin-left: 10px;
+}
+button {
+  margin: 20px auto;
+  display: block;
+  padding: 10px 15px;
+  font-size: 15px;
+  border: none;
+  border-radius: 15px;
+}
+</style>
