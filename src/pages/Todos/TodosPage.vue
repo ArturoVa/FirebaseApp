a8fa2b0ff805305f27d159b60dd75dd829aae66b
Arturo Vazquez <vazart68@gmail.com>
2023-09-03 19:26:39 -0600

    Add todos page

--- src/pages/Todos/TodosPage.vue
+++ src/pages/Todos/TodosPage.vue
@@ -1,34 +1,51 @@
-<style>
-nav{
-    display:flex;
-    background-color:whitesmoke;
-    padding-block:15px 10px;
-    justify-content: space-between;
-}
-h3{
-    margin:0;
+<script setup lang="ts">
+import { signOut } from 'firebase/auth'
+import { auth } from '@/firebase'
+import { collection, getDocs } from 'firebase/firestore'
+import { db } from '@/firebase'
+import { onMounted, ref } from 'vue'
+
+const todos = ref()
+
+async function signOutUser() {
+  await signOut(auth)
 }
 
+async function getTodos() {
+  const collectionRef = collection(db, 'todos')
+  const todosSnapshot = await getDocs(collectionRef)
+  todos.value = todosSnapshot.docs.map((doc) => {
+    return {
+      title: doc.data().title,
+      description: doc.data().description,
+      status: doc.data().status,
+      expires_at: doc.data().expires_at,
+      priority: doc.data().priority
+    }
+  })
+}
 
-</style>
+onMounted(() => {
+  getTodos() // Get Todos when the page loads for first time or after refreshing it
+})
+</script>
+
 <template>
-    
-    <div>
-        <nav>
-            <h3>Todos</h3>
-            <button @click="signOutUser">Cerrar Sesion</button>
-        </nav>
-
-    </div>
+  <nav>
+    <h3>MyTodos</h3>
+    <button @click="signOutUser">Cerrar Sesion</button>
+  </nav>
+  {{ todos }}
 </template>
 
-
-<script setup lang="ts">
-import {signOut} from 'firebase/auth';
-import {auth} from '@/firebase';
-async function signOutUser() {
-    await signOut(auth  );
-    
+<style scoped>
+nav {
+  display: flex;
+  padding: 15px 10px;
+  background-color: whitesmoke;
+  justify-content: space-between;
 }
-
-</script>
+h3 {
+  margin: 0;
+}
+</style>
