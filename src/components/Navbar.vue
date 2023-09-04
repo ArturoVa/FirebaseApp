eccbadb173fac2950441ac5a8335afdbc9ac0acd
Arturo Vazquez <vazart68@gmail.com>
2023-09-04 10:59:29 -0600

    Added Navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { signOut } from 'firebase/auth'
+import { auth } from '@/firebase'
+async function signOutUser() {
+  await signOut(auth)
+}
+</script>
+
+<template>    
+    <nav>
+        <h3>MyTodos</h3>
+        <button @click="signOutUser">Cerrar Sesion</button>
+    </nav>
+</template>
+
+
+<style scoped>
+nav {
+    display: flex;
+    padding: 15px 10px;
+    background-color: whitesmoke;
+    justify-content: space-between;
+  }
+h3 {
+    margin: 0;
+}
+
+</style>
